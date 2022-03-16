bf4ac8f18bd4413dcbe10086317aa5b21c5cadcf
root <buffie001@gmail.com>
2022-03-16 01:48:50 -0700

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
-
 /**
- * main - prints the phase "Programming is like building a multilingual puzzle
- * Return 0 if exited properly, non-zero otherwise
- * */
-int main (void)
+ * main - Entry point
+ *Return: Always 0 (Success)
+ */
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
